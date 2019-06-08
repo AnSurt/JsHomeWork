99bbc0a4e0216e688f773be95568519f460906dc
AnSurt <piffan2@gmail.com>
2019-06-08 16:12:49 +0300

    more

--- secondHomeWork/modules/cards.js
+++ secondHomeWork/modules/cards.js
@@ -1,4 +1,4 @@
-
+let allCards = [];
 let getCards = () =>
 
     fetch("http://localhost:8089/api/card")
@@ -9,28 +9,67 @@ let getCards = () =>
 
                 let column = document.getElementById(`column${element.columnId}`);
                 drawCard(element, column);
+                
 
             })
+            allCards=json;
         });
 
 
 let drawCard = (element, column) => {
     const card = document.createElement('div');
-    const deleteButton = document.createElement("button");
-    deleteButton.addEventListener('click',()=>deleteCard(element.id));
-    deleteButton.innerText= 'Remove';
-
+    const deleteButton = document.createElement("div");
+    const updatebutton = document.createElement('div');
+    updatebutton.className = 'cardButton'
+    updatebutton.innerText = 'Update'
     card.innerText = element.title;
     card.className = 'flex-card';
+    card.addEventListener('dragstart', onDragStart);
+    card.setAttribute('draggable', true);
+    card.setAttribute('data-block-id', element.id );    
     card.id = `card${element.id}`;
     card.appendChild(deleteButton);
+    card.appendChild(updatebutton);
+    deleteButton.className = 'cardButton'
+    deleteButton.addEventListener('click', () => deleteCard(element.id));
+    deleteButton.innerText = 'Remove';
+
+    card.draggable = 'true';
+
+    updatebutton.addEventListener('click', () => {
+        let newTitle = prompt('New Title');
+        updateCard({ ...element, title: newTitle })
+            .then(newCard => {
+                card.innerText = newCard.title;
+                card.appendChild(deleteButton);
+                card.appendChild(updatebutton);
+            });
+    });
+
+
     column.appendChild(card);
+    console.log(element.title , element.id);
+
+
 }
+let updateCard = (card) =>
+
+    fetch("http://localhost:8089/api/card/" + card.id, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(card)
+    })
+        .then(response => response.json())
+
+
 
 let addCard = (columnId, title) => {
-   
+
     var card = {
-        title ,
+        title,
         columnId
     };
 
@@ -39,16 +78,23 @@ let addCard = (columnId, title) => {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(card)
+        },
+        body: JSON.stringify(card)
     })
         .then(response => response.json())
         .then(element => console.log(element) || drawCard(element, document.getElementById(`column${element.columnId}`)));
 };
-let deleteCard =(id)=> fetch("http://localhost:8089/api/card/"+id.toString(), {
+let deleteCard = (id) => fetch("http://localhost:8089/api/card/" + id.toString(), {
     method: 'DELETE',
 })
-.then(()=> document.getElementById(`card${id}`).remove())
+    .then(() => document.getElementById(`card${id}`).remove())
+
+    
+
+function onDragStart(event) {
+  console.log('onDragStart', event.target.dataset.blockId);
+  event.dataTransfer.setData("blockId", event.target.dataset.blockId);
+}
 
 
-export { getCards, addCard };
+export { getCards, addCard, updateCard, allCards };
--- secondHomeWork/modules/columns.js
+++ secondHomeWork/modules/columns.js
@@ -1,26 +1,84 @@
-import { addCard } from './cards.js';
+import { addCard, updateCard, allCards } from './cards.js';
 let getColumns = () =>
-    fetch("http://localhost:8089/api/column")
-        .then(response => response.json())
-        .then(json => {
-            json.forEach(function (element) {
-                const myBoard = document.getElementById('myBoard');
-                const myColumns = document.createElement('div');
-                myColumns.innerText = element.title;
-                myColumns.className = 'flex-columns';
-                myColumns.id = `column${element.id}`;
+  fetch("http://localhost:8089/api/column")
+    .then(response => response.json())
+    .then(json => {
+      json.forEach(function (element) {
+        const myBoard = document.getElementById('myBoard');
+        const myColumns = document.createElement('div');
+        const card = document.getElementById(`card${element.id}`)
 
-                const button = document.createElement('button');
-                button.innerText = 'card';
-                button.addEventListener('click', () => {
-                    var title = prompt('Enter title');
-                    addCard(element.id, title);
-                });
-                myColumns.appendChild(button);
-                myBoard.appendChild(myColumns);
-            }
-            )
+        myColumns.innerText = element.title;
+        myColumns.className = 'flex-columns';
+        myColumns.addEventListener('dragover', onDragOver);
+        myColumns.addEventListener('drop', onDrop);
+        myColumns.id = `column${element.id}`;
+        myColumns.addEventListener('dragover', function (event) {
+          event.preventDefault();
         });
+      
+
+        const button = document.createElement('div');
+        button.innerText = '+';
+        button.className = 'columnButton'
+        button.addEventListener('click', () => {
+          var title = prompt('Enter title');
+          addCard(element.id, title);
+        });
+        myColumns.appendChild(button);
+        myBoard.appendChild(myColumns);
+
+      }
+      )
+    });
+
+
+
+function onDragOver(event) {
+  console.log('onDragover');
+  event.preventDefault()
+}
+
+function onDrop(event) {
+  try {
+    event.preventDefault()
+    console.log('onDrop');
+    const blockId = event.dataTransfer.getData("blockId");
+    console.log('dropped blockId', blockId);
+
+
+    const blockEl = document.querySelector(`[data-block-id="${blockId}"]`);
+    // blockEl.setAttribute('draggable', false);
+
+    const insertAfter = getBlockToInsertAfter(event.clientY, event.clientX);
+
+    console.log(insertAfter, blockEl);
+    if (insertAfter) {
+      insertAfter.after(blockEl)
+    } else {
+      document.querySelector('.flex-columns').append(blockEl);
+    }
+    console.log('id',event.target.id)
+    let newCard = allCards.find(card=> blockId==card.id);
+    let [newID] = event.target.id.split('column').reverse();
+    console.log("split", newID);
+   
+    console.log(allCards);
+    updateCard({...newCard, columnId: newID})
+    .then(updatedCard => allCards.splice( allCards.indexOf(newCard), 1,updatedCard))
+    .then(()=>console.log(allCards))
+ 
+
+  }
+  catch (ex) {console .log("error", ex) }
+ 
+}
+function getBlockToInsertAfter(y, x) {
+  return [...document.querySelectorAll('.flex-columns .flex-card')]
+    .filter((element) => element.getBoundingClientRect().top < y && element.getBoundingClientRect().left < x)
+    .pop();
+
+}
 
 
 export default getColumns;
